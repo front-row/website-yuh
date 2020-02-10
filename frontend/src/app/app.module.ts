55bac49b0bae487dabb8f7fb6c6493b9a033db06
kyle sadler <kylesadler5@gmail.com>
2020-02-09 21:02:14 -0600

    demo website

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DemoPageComponent } from './demo-page/demo-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DemoPageComponent
   ],
   imports: [
     BrowserModule,
